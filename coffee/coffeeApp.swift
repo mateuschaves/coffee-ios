d45337d7ab359f9f7134be0ad573bbdc9044cec3
Mateus Henrique <mateushenriquechaves1@gmail.com>
2024-03-26 21:50:07 -0300

    Initial Commit

---
+++ coffee/coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeApp.swift
+//  coffee
+//
+//  Created by Mateus Henrique on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
